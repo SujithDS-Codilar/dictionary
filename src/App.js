205b002dde4cd2bf6804985752042763a504ad58
SujithDS-Codilar <sujith.ds@codilar.com>
2021-12-30 10:43:32 +0530

    Modified Task

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ class App extends Component {
   //   })
   //   this.fetchapi1()
   // }
+
+
   fetchapi1 = (e) => {
     e.preventDefault();
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.state.searchdata}`)
@@ -28,62 +30,79 @@ class App extends Component {
         console.log(json);
         this.setState({
           items: json,
-
         });
       }).catch(error => {
-        console.log(error);
-
+        alert(error);
       })
   }
 
-
-
   renderMeaning(meaning) {
     return (
-      <li>
-        <strong>{meaning.partOfSpeech}</strong>
-        <ul>
-          {
-            this.renderDefinations(meaning.definitions)
-          }
-        </ul>
-      </li>
+      <ol>
+        <li className="Speech">
+          {/* <span>{i+1}</span> */}
+          <i>{meaning.partOfSpeech}</i>
+          <li>
+            {
+              this.renderDefinations(meaning.definitions)
+            }
+          </li>
+        </li>
+      </ol>
     )
   }
 
   renderDefinations(definations) {
     return (
-      definations.map(meaning => {
+      definations.map((meaning, i) => {
+        // console.log(i);
+
         return (
-          <li>
-            {meaning.definition}
+          <div>
+            <ol>
+              <li key={i}>
+                <span className="Digits">{i + 1} </span>
+                {meaning.definition}
+
+              </li>
+              <li key={i * 100}>
+                <p>
+                  {/* <h4>Example </h4> */}
+                  {typeof (meaning.example) !== 'undefined' && <> <strong className="Example__Text">"{meaning.example}"</strong></>}
+                  {/* {console.log(meaning.example)} */}
+
+                </p>
+              </li>
+            </ol>
+
+
             <p>
-              <strong>{meaning.example}</strong>
+              {meaning.synonyms.length !== 0 && <span className="Text">Similar: </span>}
+              {/* <span className="Text">Similar:</span> */}
+
+              {this.rendersynonyms(meaning.synonyms)}
             </p>
+
+          </div>
+        )
+      })
+    )
+  }
+
+  rendersynonyms = (synonyms) => {
+    return (
+      synonyms.map((synonym) => {
+        return (
+          <li className="synonymss">
             <ul>
-             <li>  <h3>synonyms</h3>
-            
-            {this.rendersynonyms(meaning.synonyms)}
-            </li>
+              <li >
+                {synonym}
+              </li>
             </ul>
           </li>
         )
       })
     )
-  }
-  rendersynonyms =(synonyms) => {
-    return(
-
-    
-    synonyms.map((synonym)=>{
-      return(
-
-      <li>
-         {synonym}
-      </li>
-      )
-    })
-    ) 
   }
 
 
@@ -91,26 +110,21 @@ class App extends Component {
     console.log(this.state)
     return (
       <div className="Search__Bar">
-        <div className="Title__Head">Dictionary App using React</div>
+        <h1>Dictionary</h1>
         <form onSubmit={this.fetchapi1}>
-          <div className="Input__Field"><input className="Input__Type" type="search" placeholder="Type Something" onChange={(e) => {
+          <div className="Input__Field"><input type="text" placeholder="Search for a word" onChange={(e) => {
             e.preventDefault();
             var a = e.target.value;
             this.setState({
               searchdata: a,
-
             })
-
             console.log(this.state.searchdata);
           }}></input>
-
           </div>
           <button type="submit">
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z" b /></svg>
           </button>
         </form>
-
-
         <div>
           {this.state.items.map((item, i) => {
             let { word } = item
@@ -118,19 +132,19 @@ class App extends Component {
               <ul style={{
                 listStyle: "none"
               }}>
-                <li key={i}><h1>{word}</h1></li>
-                <li key={i}>{item.phonetic}</li>
-                <li key={i}><button onClick={() => { new Audio(item.phonetics[0].audio).play() }}>Audio</button></li>
+                <li key={i}><img src="volume3-2.svg" width="1.5%" onClick={() => { new Audio(item.phonetics[0].audio).play() }} alt="" /><span>{word}</span></li>
+                <li className="phonetics" key={i}>/{item.phonetic}/</li><br></br>
+                {/* <li key={i}><button onClick={() => { new Audio(item.phonetics[0].audio).play() }}>Audio</button></li> */}
                 <li key={i}>
                   <ul>
                     {
                       item.meanings.map((meaning) => {
                         return this.renderMeaning(meaning)
+                        // console.log(i);
                       })
                     }
                   </ul>
                 </li>
-
               </ul>
             )
           })}
